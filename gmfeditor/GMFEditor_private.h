dd3e2a1160e808dd86755c6578c02158ee793949
Bazanski <Bazanski@1d10bff2-9961-11dd-bd51-a78b85c98fae>
2009-03-06 20:21:39 +0000

    New stuff.

--- gmfeditor/GMFEditor_private.h
+++ gmfeditor/GMFEditor_private.h
@@ -5,11 +5,11 @@
 #define GMFEDITOR_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.1.1.3"
+#define VER_STRING	"0.1.1.415"
 #define VER_MAJOR	0
 #define VER_MINOR	1
 #define VER_RELEASE	1
-#define VER_BUILD	3
+#define VER_BUILD	415
 #define COMPANY_NAME	"Sergiusz Bazanski q3k.org"
 #define FILE_VERSION	""
 #define FILE_DESCRIPTION	"A tool to explore GMF files"
